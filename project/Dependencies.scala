96638b56fad83e121a6cc5ee18bae9edf5a4ccc8
Ana <ana@47deg.com>
2015-11-12 12:32:04 +0100

    am-9C-330: Indenting of code

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ trait Dependencies {
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % Versions.akka
   val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % Versions.akka
   val specs2Core = "org.specs2" %% "specs2-core" % Versions.specs2
-  val sprayJson = "io.spray" %%  "spray-json" % Versions.sprayJson
+  val sprayJson = "io.spray" %% "spray-json" % Versions.sprayJson
 
   val baseDepts = Seq(specs2Core % "test")
 
