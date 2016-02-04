ba292526010ec4ea797146415c171a9daab49d4d
franciscodr <francisco.d@47deg.com>
2016-02-04 13:19:54 +0100

    9C-200: Gets database connection info from config file

--- modules/services/src/main/scala/com/fortysevendeg/ninecards/services/persistence/package.scala
+++ modules/services/src/main/scala/com/fortysevendeg/ninecards/services/persistence/package.scala
@@ -1,5 +1,6 @@
 package com.fortysevendeg.ninecards.services
 
+import com.fortysevendeg.ninecards.services.common.NineCardsConfig._
 import doobie.imports._
 
 import scalaz.concurrent.Task
@@ -7,9 +8,9 @@ import scalaz.concurrent.Task
 package object persistence {
 
   val transactor = DriverManagerTransactor[Task](
-    driver = "org.postgresql.Driver",
-    url = "jdbc:postgresql:ninecards",
-    user = "postgres",
-    pass = "postgres")
+    driver = getConfigValue("db.default.driver"),
+    url = getConfigValue("db.default.url"),
+    user = getConfigValue("db.default.user"),
+    pass = getConfigValue("db.default.password"))
 
 }
--- project/Versions.scala
+++ project/Versions.scala
@@ -5,13 +5,14 @@ object Versions {
 
   // Core Libs
   val akka = "2.3.12"
-  val cats = "0.4.0-SNAPSHOT"
+  val cats = "0.4.0"
   val doobie = "0.2.4-SNAPSHOT"
   val jodaTime = "2.9.1"
   val scala = "2.11.7"
   val scalaz = "7.1.5"
   val spray = "1.3.3"
   val sprayJson = "1.3.2"
+  val typesafeConfig = "1.3.0"
 
   // Testing libs
   val specs2 = "3.6.5"
