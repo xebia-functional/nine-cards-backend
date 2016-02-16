6b6ea32e3814ba12366d40665e36c8ee2ce1342f
Ana <ana@47deg.com>
2016-02-16 14:44:47 +0100

    9C-295: Added new test for getInstallationByUserAndAndroidId and getUserByEmail

--- modules/services/src/test/scala/com/fortysevendeg/ninecards/services/persistence/UserPersistenceImplSpec.scala
+++ modules/services/src/test/scala/com/fortysevendeg/ninecards/services/persistence/UserPersistenceImplSpec.scala
@@ -37,7 +37,6 @@ class UserPersistenceImplSpec
 
   implicit val arbInstallation: Arbitrary[Installation] = Arbitrary(installationGenerator)
 
-
   "addUser" should {
     "new users can be created" in {
       prop { (user: User) =>
@@ -51,7 +50,13 @@ class UserPersistenceImplSpec
   }
 
   "getUserByEmail" should {
-    "users can be queried by their Email" in {
+    "return None if the table is empty" in {
+      prop { (user: User) =>
+        val storeUser = userPersistenceServices.getUserByEmail(user.email).transact(transactor).run
+        storeUser should beNone
+      }
+    }
+    "return an user if there is an user with the given email in the database" in {
       prop { (user: User) =>
         val id: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
         val storeUser = userPersistenceServices.getUserByEmail(user.email).transact(transactor).run
@@ -60,30 +65,63 @@ class UserPersistenceImplSpec
         }
       }
     }
+    "return None if there isn't any user with the given email in the database" in {
+      prop { (user: User) =>
+        val id: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
+        val storeUser = userPersistenceServices.getUserByEmail(user.email.reverse).transact(transactor).run
+        storeUser should beNone
+      }
+    }
   }
 
   "createInstallation" should {
     "new installation can be created" in {
-      prop { (installation: Installation, user: User) =>
-        val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-        val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
-        val storeInstallation = userPersistenceServices.getInstallationById(id = id).transact(transactor).run
-        storeInstallation should beSome[Installation].which {
-          install => install shouldEqual Installation(id= id, userId =userId, deviceToken = None,androidId = installation.androidId)
-        }
+      prop {
+        (installation: Installation, user: User) =>
+          val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
+          val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
+          val storeInstallation = userPersistenceServices.getInstallationById(id = id).transact(transactor).run
+          storeInstallation should beSome[Installation].which {
+            install => install shouldEqual Installation(id = id, userId = userId, deviceToken = None, androidId = installation.androidId)
+          }
       }
     }
   }
 
   "getInstallationByUserAndAndroidId" should {
+    "return None if the table is empty" in {
+      prop { (installation: Installation) =>
+        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = installation.userId, androidId = installation.androidId).transact(transactor).run
+        storeInstallation should beNone
+      }
+    }
     "installations can be queried by their userId and androidId" in {
+      prop {
+        (installation: Installation, user: User) =>
+          val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
+          val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
+          val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = installation.androidId).transact(transactor).run
+          storeInstallation should beSome[Installation].which {
+            install => install.id shouldEqual id
+          }
+      }
+    }
+    "return None if there isn't any installation with the given userId and androidId in the database" in {
       prop { (installation: Installation, user: User) =>
         val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
         val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
-        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = installation.androidId).transact(transactor).run
-        storeInstallation should beSome[Installation].which {
-          install => install.id shouldEqual id
-        }
+        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = installation.androidId.reverse).transact(transactor).run
+        storeInstallation should beNone
+      }
+    }
+  }
+
+  "getInstallationByUserAndAndroidId" should {
+    "return None if the table is empty" in {
+      prop {
+        (installation: Installation, user: User) =>
+          val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = user.id, androidId = installation.androidId).transact(transactor).run
+          storeInstallation should beNone
       }
     }
   }
