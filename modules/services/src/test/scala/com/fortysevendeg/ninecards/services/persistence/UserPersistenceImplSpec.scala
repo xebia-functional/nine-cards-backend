447e177c9170b67b686459ae9ab728deae81cb8e
Ana <ana@47deg.com>
2016-02-18 13:52:43 +0100

    9C-295: Solves comments in PR. I removed the userGenerator and installationGenerator because Paco created in other class,  case class for use in this case.

--- modules/services/src/test/scala/com/fortysevendeg/ninecards/services/persistence/UserPersistenceImplSpec.scala
+++ modules/services/src/test/scala/com/fortysevendeg/ninecards/services/persistence/UserPersistenceImplSpec.scala
@@ -1,18 +1,21 @@
 package com.fortysevendeg.ninecards.services.persistence
 
+
 import com.fortysevendeg.ninecards.services.free.domain.{Installation, User}
+import com.fortysevendeg.ninecards.services.persistence.NineCardsGenEntities.{AndroidId, SessionToken, Email}
 import org.specs2.ScalaCheck
 import doobie.imports._
 import org.specs2.mutable.Specification
 import org.scalacheck.{Arbitrary, Gen}
 import org.specs2.specification.BeforeEach
-
+import org.scalacheck.Arbitrary.arbitrary
 
 class UserPersistenceImplSpec
   extends Specification
     with BeforeEach
     with ScalaCheck
-    with DomainDatabaseContext {
+    with DomainDatabaseContext
+    with NineCardsScalacheckGen {
 
   sequential
 
@@ -21,29 +24,13 @@ class UserPersistenceImplSpec
     flywaydb.migrate()
   }
 
-  val userGenerator: Gen[User] = for {
-    id <- Gen.resultOf((s: Long) => (s))
-    email <- Gen.uuid
-    sessionToken <- Gen.uuid
-  } yield User(id = id, email = email.toString, sessionToken = sessionToken.toString, banned = false)
-
-  implicit val arbUser: Arbitrary[User] = Arbitrary(userGenerator)
-
-  val installationGenerator: Gen[Installation] = for {
-    id <- Gen.resultOf((s: Long) => (s))
-    userId <- Gen.resultOf((s: Long) => (s))
-    androidId <- Gen.uuid
-  } yield Installation(id = id, userId = userId, deviceToken = None, androidId = androidId.toString)
-
-  implicit val arbInstallation: Arbitrary[Installation] = Arbitrary(installationGenerator)
-
   "addUser" should {
     "new users can be created" in {
-      prop { (user: User) =>
-        val id: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-        val storeUser = userPersistenceServices.getUserByEmail(user.email).transact(transactor).run
+      prop { (email: Email, sessionToken: SessionToken) =>
+        val id: Long = userPersistenceServices.addUser[Long](email.value, sessionToken.value).transact(transactor).run
+        val storeUser = userPersistenceServices.getUserByEmail(email.value).transact(transactor).run
         storeUser should beSome[User].which {
-          u => u == user.copy(id)
+          user => user.email shouldEqual email.value
         }
       }
     }
@@ -51,24 +38,24 @@ class UserPersistenceImplSpec
 
   "getUserByEmail" should {
     "return None if the table is empty" in {
-      prop { (user: User) =>
-        val storeUser = userPersistenceServices.getUserByEmail(user.email).transact(transactor).run
+      prop { (email: Email) =>
+        val storeUser = userPersistenceServices.getUserByEmail(email.value).transact(transactor).run
         storeUser should beNone
       }
     }
     "return an user if there is an user with the given email in the database" in {
-      prop { (user: User) =>
-        val id: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-        val storeUser = userPersistenceServices.getUserByEmail(user.email).transact(transactor).run
+      prop { (email: Email, sessionToken: SessionToken) =>
+        val id: Long = userPersistenceServices.addUser[Long](email.value, sessionToken.value).transact(transactor).run
+        val storeUser = userPersistenceServices.getUserByEmail(email.value).transact(transactor).run
         storeUser should beSome[User].which {
-          user2 => user.email shouldEqual user2.email
+          user => user shouldEqual User(id = id, email = email.value, sessionToken = sessionToken.value, banned = false)
         }
       }
     }
     "return None if there isn't any user with the given email in the database" in {
-      prop { (user: User) =>
-        val id: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-        val storeUser = userPersistenceServices.getUserByEmail(user.email.reverse).transact(transactor).run
+      prop { (email: Email, sessionToken: SessionToken) =>
+        val id: Long = userPersistenceServices.addUser[Long](email.value, sessionToken.value).transact(transactor).run
+        val storeUser = userPersistenceServices.getUserByEmail(email.value.reverse).transact(transactor).run
         storeUser should beNone
       }
     }
@@ -76,41 +63,39 @@ class UserPersistenceImplSpec
 
   "createInstallation" should {
     "new installation can be created" in {
-      prop {
-        (installation: Installation, user: User) =>
-          val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-          val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
-          val storeInstallation = userPersistenceServices.getInstallationById(id = id).transact(transactor).run
-          storeInstallation should beSome[Installation].which {
-            install => install shouldEqual Installation(id = id, userId = userId, deviceToken = None, androidId = installation.androidId)
-          }
+      prop { (androidId: AndroidId, email: Email, sessionToken: SessionToken) =>
+        val userId: Long = userPersistenceServices.addUser[Long](email.value, sessionToken.value).transact(transactor).run
+        val id = userPersistenceServices.createInstallation[Long](userId, None, androidId.value).transact(transactor).run
+        val storeInstallation = userPersistenceServices.getInstallationById(id = id).transact(transactor).run
+        storeInstallation should beSome[Installation].which {
+          install => install shouldEqual Installation(id = id, userId = userId, deviceToken = None, androidId = androidId.value)
+        }
       }
     }
   }
 
   "getInstallationByUserAndAndroidId" should {
     "return None if the table is empty" in {
-      prop { (installation: Installation) =>
-        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = installation.userId, androidId = installation.androidId).transact(transactor).run
+      prop { (androidId: AndroidId, userId: Long) =>
+        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = androidId.value).transact(transactor).run
         storeInstallation should beNone
       }
     }
     "installations can be queried by their userId and androidId" in {
-      prop {
-        (installation: Installation, user: User) =>
-          val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-          val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
-          val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = installation.androidId).transact(transactor).run
-          storeInstallation should beSome[Installation].which {
-            install => install.id shouldEqual id
-          }
+      prop { (androidId: AndroidId, email: Email, sessionToken: SessionToken) =>
+        val userId: Long = userPersistenceServices.addUser[Long](email.value, sessionToken.value).transact(transactor).run
+        val id = userPersistenceServices.createInstallation[Long](userId, None, androidId.value).transact(transactor).run
+        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = androidId.value).transact(transactor).run
+        storeInstallation should beSome[Installation].which {
+          install => install.id shouldEqual id
+        }
       }
     }
     "return None if there isn't any installation with the given userId and androidId in the database" in {
-      prop { (installation: Installation, user: User) =>
-        val userId: Long = userPersistenceServices.addUser[Long](user.email, user.sessionToken).transact(transactor).run
-        val id = userPersistenceServices.createInstallation[Long](userId, None, installation.androidId).transact(transactor).run
-        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = installation.androidId.reverse).transact(transactor).run
+      prop { (androidId: AndroidId, email: Email, sessionToken: SessionToken) =>
+        val userId: Long = userPersistenceServices.addUser[Long](email.value, sessionToken.value).transact(transactor).run
+        val id = userPersistenceServices.createInstallation[Long](userId, None, androidId.value).transact(transactor).run
+        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = androidId.value.reverse).transact(transactor).run
         storeInstallation should beNone
       }
     }
@@ -118,10 +103,9 @@ class UserPersistenceImplSpec
 
   "getInstallationByUserAndAndroidId" should {
     "return None if the table is empty" in {
-      prop {
-        (installation: Installation, user: User) =>
-          val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = user.id, androidId = installation.androidId).transact(transactor).run
-          storeInstallation should beNone
+      prop { (androidId: AndroidId, userId: Long) =>
+        val storeInstallation = userPersistenceServices.getInstallationByUserAndAndroidId(userId = userId, androidId = androidId.value).transact(transactor).run
+        storeInstallation should beNone
       }
     }
   }
