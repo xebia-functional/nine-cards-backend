c251dc24e0bcf2e3381523d8af82d984151b89b9
Noel Markham <noel.markham@gmail.com>
2016-02-18 09:10:58 +0000

    Including our own Google Play protobuf file so that we can eventually parse Play Store responses without using the crawler library.

--- project/NineCardsGooglePlayBuild.scala
+++ project/NineCardsGooglePlayBuild.scala
@@ -1,9 +1,12 @@
 import sbt._
+import Keys._
+
+import sbtprotobuf.{ProtobufPlugin=>PB}
 
 object NineCardsBuild extends Build with Settings with Dependencies {
 
   lazy val root = project.in(file(".")) aggregate(api)
 
   lazy val api = project.in(file("modules/api"))
-    .settings(apiSettings ++ apiDeps)
+    .settings(apiSettings ++ apiDeps ++ PB.protobufSettings : _*)
 }
