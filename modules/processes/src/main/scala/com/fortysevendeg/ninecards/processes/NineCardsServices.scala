b7d483a12ef93dd43dc28fa98e5ee1003576cad4
Ana <ana@47deg.com>
2016-03-02 14:56:46 +0100

    9C-322: Cleaned code. Removed class that we doesn't use.

--- modules/processes/src/main/scala/com/fortysevendeg/ninecards/processes/NineCardsServices.scala
+++ modules/processes/src/main/scala/com/fortysevendeg/ninecards/processes/NineCardsServices.scala
@@ -2,8 +2,6 @@ package com.fortysevendeg.ninecards.processes
 
 import cats.data.Coproduct
 import cats.~>
-import com.fortysevendeg.ninecards.services.free.algebra.AppGooglePlay.AppGooglePlayOps
-import com.fortysevendeg.ninecards.services.free.algebra.AppPersistence.AppPersistenceOps
 import com.fortysevendeg.ninecards.services.free.algebra.DBResult.DBResult
 import com.fortysevendeg.ninecards.services.free.algebra.SharedCollectionSubscriptions.SharedCollectionSubscriptionOps
 import com.fortysevendeg.ninecards.services.free.algebra.SharedCollections.SharedCollectionOps
@@ -13,14 +11,10 @@ import scalaz.concurrent.Task
 
 object NineCardsServices {
 
-  type ServicesCO3[A] = Coproduct[SharedCollectionOps, SharedCollectionSubscriptionOps, A]
-  type ServicesCO2[A] = Coproduct[AppPersistenceOps, ServicesCO3, A]
-  type ServicesCO1[A] = Coproduct[AppGooglePlayOps, ServicesCO2, A]
+  type ServicesCO1[A] = Coproduct[SharedCollectionOps, SharedCollectionSubscriptionOps, A]
   type NineCardsServices[A] = Coproduct[DBResult, ServicesCO1, A]
 
-  val interpretersCO3: ServicesCO3 ~> Task = SharedCollectionInterpreter or SharedCollectionSubscriptionInterpreter
-  val interpretersCO2: ServicesCO2 ~> Task = AppPersistenceInterpreter or interpretersCO3
-  val interpretersCO1: ServicesCO1 ~> Task = AppGooglePlayInterpreter or interpretersCO2
+  val interpretersCO1: ServicesCO1 ~> Task = SharedCollectionInterpreter or SharedCollectionSubscriptionInterpreter
   val interpreters: NineCardsServices ~> Task = DBResultInterpreter or interpretersCO1
 
 }
