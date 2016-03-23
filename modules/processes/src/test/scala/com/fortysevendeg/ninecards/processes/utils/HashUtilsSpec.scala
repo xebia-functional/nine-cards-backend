6a33f569baccf84c94e102954be72143a8405bcb
franciscodr <francisco.d@47deg.com>
2016-03-23 11:20:56 +0100

    9C-266: Using Scalacheck in HashUtilSpec class

--- modules/processes/src/test/scala/com/fortysevendeg/ninecards/processes/utils/HashUtilsSpec.scala
+++ modules/processes/src/test/scala/com/fortysevendeg/ninecards/processes/utils/HashUtilsSpec.scala
@@ -1,53 +1,83 @@
 package com.fortysevendeg.ninecards.processes.utils
 
 import com.roundeights.hasher.Hasher
+import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
-class HashUtilsSpec extends Specification with DummyNineCardsConfig {
+class HashUtilsSpec
+  extends Specification
+    with DummyNineCardsConfig
+    with ScalaCheck {
 
   val hashUtils = new HashUtils
-  val text = "Hash me!"
-  val hasher = Hasher(text).salt(nineCardsSalt)
-  val md5HashValue = hasher.md5.hex
-  val sha256HashValue = hasher.sha256.hex
-  val sha512HashValue = hasher.sha512.hex
-  val hMacMd5HashValue = hasher.hmac(nineCardsSecretKey).md5.hex
-  val hMacSha256HashValue = hasher.hmac(nineCardsSecretKey).sha256.hex
-  val hMacSha512HashValue = hasher.hmac(nineCardsSecretKey).sha512.hex
 
   "hashValue" should {
     "return a hMacSha512 hash value if no algorithm is specified" in {
-      hashUtils.hashValue(text = text) shouldEqual hMacSha512HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).hmac(nineCardsSecretKey).sha512.hex
+
+        hashUtils.hashValue(text = text) shouldEqual expectedHashValue
+      }
     }
+
     "return a md5 hash value if md5 algorithm is specified" in {
-      hashUtils.hashValue(
-        text = text,
-        algorithm = EncryptionAlgorithm.Md5) shouldEqual md5HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).md5.hex
+
+        hashUtils.hashValue(
+          text = text,
+          algorithm = EncryptionAlgorithm.Md5) shouldEqual expectedHashValue
+      }
     }
+
     "return a sha256 hash value if sha256 algorithm is specified" in {
-      hashUtils.hashValue(
-        text = text,
-        algorithm = EncryptionAlgorithm.Sha256) shouldEqual sha256HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).sha256.hex
+
+        hashUtils.hashValue(
+          text = text,
+          algorithm = EncryptionAlgorithm.Sha256) shouldEqual expectedHashValue
+      }
     }
+
     "return a sha512 hash value if sha512 algorithm is specified" in {
-      hashUtils.hashValue(
-        text = text,
-        algorithm = EncryptionAlgorithm.Sha512) shouldEqual sha512HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).sha512.hex
+
+        hashUtils.hashValue(
+          text = text,
+          algorithm = EncryptionAlgorithm.Sha512) shouldEqual expectedHashValue
+      }
     }
+
     "return a hMacMd5 hash value if hMacMd5 algorithm is specified" in {
-      hashUtils.hashValue(
-        text = text,
-        algorithm = EncryptionAlgorithm.HMacMd5) shouldEqual hMacMd5HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).hmac(nineCardsSecretKey).md5.hex
+
+        hashUtils.hashValue(
+          text = text,
+          algorithm = EncryptionAlgorithm.HMacMd5) shouldEqual expectedHashValue
+      }
     }
+
     "return a hMacSha256 hash value if hMacSha256 algorithm is specified" in {
-      hashUtils.hashValue(
-        text = text,
-        algorithm = EncryptionAlgorithm.HMacSha256) shouldEqual hMacSha256HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).hmac(nineCardsSecretKey).sha256.hex
+
+        hashUtils.hashValue(
+          text = text,
+          algorithm = EncryptionAlgorithm.HMacSha256) shouldEqual expectedHashValue
+      }
     }
+
     "return a hMacSha512 hash value if hMacSha512 algorithm is specified" in {
-      hashUtils.hashValue(
-        text = text,
-        algorithm = EncryptionAlgorithm.HMacSha512) shouldEqual hMacSha512HashValue
+      prop { (text: String) =>
+        val expectedHashValue = Hasher(text).salt(nineCardsSalt).hmac(nineCardsSecretKey).sha512.hex
+
+        hashUtils.hashValue(
+          text = text,
+          algorithm = EncryptionAlgorithm.HMacSha512) shouldEqual expectedHashValue
+      }
     }
   }
 }
