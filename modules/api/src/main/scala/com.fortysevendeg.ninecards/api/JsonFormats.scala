17ab888206168d2d828477c88af0a01c703033f7
franciscodr <francisco.d@47deg.com>
2016-04-01 10:26:29 +0200

    9C-179: Uses Circe to encode and decode DateTime values

--- modules/api/src/main/scala/com.fortysevendeg.ninecards/api/JsonFormats.scala
+++ modules/api/src/main/scala/com.fortysevendeg.ninecards/api/JsonFormats.scala
@@ -1,42 +1,44 @@
 package com.fortysevendeg.ninecards.api
 
+import cats.data.Xor
 import com.fortysevendeg.ninecards.api.messages.InstallationsMessages._
 import com.fortysevendeg.ninecards.api.messages.SharedCollectionMessages._
 import com.fortysevendeg.ninecards.api.messages.UserMessages._
 import com.fortysevendeg.ninecards.processes.domain._
+import io.circe.{Decoder, Encoder, Json}
 import org.joda.time.DateTime
-import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.format.DateTimeFormat
 import spray.httpx.SprayJsonSupport
 import spray.json._
-
-import scala.util.{Success, Try}
 
 trait JsonFormats
   extends DefaultJsonProtocol
     with SprayJsonSupport {
 
-  implicit object DateTimeFormat extends RootJsonFormat[DateTime] {
+  implicit object JodaDateTimeFormat extends RootJsonFormat[DateTime] {
+    val formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
+    val dateExample = formatter.print(0)
 
-    val formatter = ISODateTimeFormat.basicDateTime
+    def error(v: String) = deserializationError(
+      s"'$v' is not a valid date value. Dates must be in compact ISO-8601 format, e.g. '$dateExample'")
 
-    def write(obj: DateTime): JsValue = {
-      JsString(formatter.print(obj))
+    val decodeDateTime: Decoder[DateTime] = Decoder.instance { cursor =>
+      cursor.as[String].flatMap {
+        case dateTime => Xor.right(DateTime.parse(dateTime, formatter))
+      }
     }
+
+    val encodeDateTime: Encoder[DateTime] = Encoder.instance { dateTime: DateTime =>
+      Json.string(formatter.print(dateTime))
+    }
+
+    def write(obj: DateTime): JsValue = JsString(encodeDateTime(obj).noSpaces)
 
     def read(json: JsValue): DateTime = json match {
-      case JsString(s) =>
-        Try(formatter.parseDateTime(s)) match {
-          case Success(d) => d
-          case _ => error(s)
-        }
-      case _ =>
-        error(json.toString)
+      case JsString(s) => decodeDateTime(Json.string(s).hcursor).fold(error(s), d => d)
+      case _ => error(json.toString)
     }
 
-    def error(v: Any): DateTime = {
-      val example = formatter.print(0)
-      deserializationError(f"'$v' is not a valid date value. Dates must be in compact ISO-8601 format, e.g. '$example'")
-    }
   }
 
   implicit val googlePlayAppFormat = jsonFormat7(GooglePlayApp)
