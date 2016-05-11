45863c896fcdfd548c249a0a889acd3deaa7ddf8
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2016-05-11 09:47:36 +0100

    9C-450 Improve documentation and installation.
    **README** file: We have completed the README with
    
    * We give an overview of the backend's architecture.
    * We describe the data model and the main tables of the application.
    * We add some details about how to configure the PostgreSQL system, and
      how to use the Flyway to perform the database evolutions.
    * We explain how to perform the authentication and authorization using
      OAuth 2.0 with the Google account server, and how to generate the
      cryptographical key.
    
    **Configuration**
      We add a special `localhost.conf` configuration file for localhost
      running.
    
    **Postman** we have updated the collection of requests for the Postman
       endpoint. We also add an environment with the typical values for
       local execution and testing.
    
    **Flyway**: We change the FlywayConfig, so that it loads the database
      connection configuration from the `conf` file passed to the SBT.
      Before this, the use of `application.conf` was hard-coded.

--- project/FlywayConfig.scala
+++ project/FlywayConfig.scala
@@ -1,35 +1,24 @@
-import com.typesafe.config._
-import sbt.Keys._
-import sbt._
+import com.typesafe.config.ConfigFactory
+import sbt.{Def, settingKey}
 
 object FlywayConfig {
 
   case class DatabaseConfig(driver: String, url: String, user: String, password: String)
 
-  class CustomConfig(fileName: File) {
-
-    val config = ConfigFactory.parseFile(fileName).resolve
-
-    def envOrElseConfig(name: String): String = {
-      sys.props.getOrElse(
-        key = name,
-        default = config.getString(name)
-      )
-    }
-  }
-
   lazy val databaseConfig = settingKey[DatabaseConfig]("The database config to use in Flyway")
 
   lazy val databaseConfigDef: Def.Initialize[DatabaseConfig] =
     Def.setting[DatabaseConfig] {
-      val config = new CustomConfig((resourceDirectory in Compile).value / "application.conf")
+      val config = ConfigFactory.load()
       val prefix = "db.default"
 
+      def envOrElseConfig(name: String) = sys.props.get(name) getOrElse config.getString(name)
+
       DatabaseConfig(
-        driver = config.envOrElseConfig(s"$prefix.driver"),
-        url = config.envOrElseConfig(s"$prefix.url"),
-        user = config.envOrElseConfig(s"$prefix.user"),
-        password = config.envOrElseConfig(s"$prefix.password")
+        driver = envOrElseConfig(s"$prefix.driver"),
+        url = envOrElseConfig(s"$prefix.url"),
+        user = envOrElseConfig(s"$prefix.user"),
+        password = envOrElseConfig(s"$prefix.password")
       )
     }
 }
