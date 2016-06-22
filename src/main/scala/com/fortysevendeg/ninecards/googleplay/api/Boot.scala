589b5b1d6bb07b4cae45f7beebdc68aee2068532
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2016-06-22 19:41:14 +0100

    9C-253 Extract a Wiring Section

--- src/main/scala/com/fortysevendeg/ninecards/googleplay/api/Boot.scala
+++ src/main/scala/com/fortysevendeg/ninecards/googleplay/api/Boot.scala
@@ -4,17 +4,37 @@ import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
-import spray.can.Http
-
-import com.fortysevendeg.extracats._
+import cats.~>
 import com.fortysevendeg.ninecards.config.NineCardsConfig.getConfigValue
+import com.fortysevendeg.ninecards.googleplay.service.free.algebra.GooglePlay.GooglePlayOps
+import com.fortysevendeg.ninecards.googleplay.service.free.interpreter._
+import com.redis.RedisClientPool
+import org.http4s.client.blaze.PooledHttp1Client
 import scala.concurrent.duration._
+import scalaz.concurrent.Task
+import spray.can.Http
 
 object Boot extends App {
 
   implicit private val system = ActorSystem("nine-cards-google-play-server-actor")
 
-  private val service = system.actorOf(Props[NineCardsGooglePlayActor], "nine-cards-google-play-server")
+  private val interpreter: GooglePlayOps ~> Task = {
+    val httpClient = PooledHttp1Client()
+    val redisPool = new RedisClientPool(
+      host = getConfigValue("googleplay.cache.host"),
+      port = getConfigValue("googleplay.cache.port").toInt
+    )
+    val apiClient = new Http4sGooglePlayApiClient(  getConfigValue("googleplay.api.endpoint"), httpClient)
+    val webClient = new Http4sGooglePlayWebScraper( getConfigValue("googleplay.web.endpoint"), httpClient)
+    val cachedApiClient = new CachedQueryService( "apiClient", apiClient, redisPool)
+    val cachedWebScrape = new CachedQueryService( "webScrape", webClient, redisPool)
+    TaskInterpreter(cachedApiClient, cachedWebScrape)
+  }
+
+  private val service = system.actorOf(
+    Props( classOf[NineCardsGooglePlayActor], interpreter),
+    "nine-cards-google-play-server"
+  )
 
   implicit private val timeout = Timeout(5.seconds)
 
--- src/main/scala/com/fortysevendeg/ninecards/googleplay/api/NineCardsGooglePlayApi.scala
+++ src/main/scala/com/fortysevendeg/ninecards/googleplay/api/NineCardsGooglePlayApi.scala
@@ -4,10 +4,8 @@ import akka.actor.Actor
 import cats.Monad
 import cats.~>
 import com.fortysevendeg.extracats._
-import com.fortysevendeg.ninecards.config.NineCardsConfig.getConfigValue
 import com.fortysevendeg.ninecards.googleplay.domain.Domain._
 import com.fortysevendeg.ninecards.googleplay.service.free.algebra.GooglePlay._
-import com.fortysevendeg.ninecards.googleplay.service.free.interpreter.{ Http4sGooglePlayApiClient, Http4sGooglePlayWebScraper, TaskInterpreter }
 import io.circe.generic.auto._
 import org.http4s.client.Client
 import org.http4s.client.blaze.PooledHttp1Client
@@ -15,23 +13,13 @@ import scalaz.concurrent.Task
 import spray.httpx.marshalling.ToResponseMarshaller
 import spray.routing.{Directives, HttpService, Route}
 
-class NineCardsGooglePlayActor extends Actor with HttpService {
+class NineCardsGooglePlayActor( googlePlayInterpreter: GooglePlayOps ~> Task) extends Actor with HttpService {
 
   override def actorRefFactory = context
 
-  // todo make a new wiring module
-
   private val googleApiRoute: Route = {
     import NineCardsMarshallers._
-
-    // todo make a new wiring module
-    implicit val inter: GooglePlayOps ~> Task = {
-      val client = PooledHttp1Client()
-      val apiClient = new Http4sGooglePlayApiClient(  getConfigValue("googleplay.api.endpoint"), client)
-      val webClient = new Http4sGooglePlayWebScraper( getConfigValue("googleplay.web.endpoint"), client)
-      TaskInterpreter(apiClient, webClient)
-    }
-
+    implicit val inter: GooglePlayOps ~> Task = googlePlayInterpreter
     NineCardsGooglePlayApi.googlePlayApiRoute[Task]
   }
 
