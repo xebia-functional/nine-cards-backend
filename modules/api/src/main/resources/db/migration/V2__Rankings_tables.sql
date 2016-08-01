78247257d8b7d543186c42fbe0f556e75faa4d8b
Diego Esteban Alonso Blas <diesalbla@gmail.com>
2016-08-01 15:43:49 +0200

    9C-470 Create Rankings Tables
    We create the tables for apps rankings. The ticket asks for a separate
    table for each geographical scope (world-continent-country).
    To avoid repeating code, we implement the migration with a simple
    procedure, that we invoke with each scope.

---
+++ modules/api/src/main/resources/db/migration/V2__Rankings_tables.sql
@@ -0,0 +1,29 @@
+
+create or replace function create_ranking_table( geo_scope varchar(30)) returns VOID  as $$
+begin
+  execute format(
+    ' create table if not exists %I (
+        packageName character  varying(256) not null,
+        category varchar(30) not null,
+        ranking integer not null,
+        primary key (packageName, category)
+    )',
+    'Rankings_' || geo_scope
+    );
+end
+$$ LANGUAGE plpgsql;
+
+select 1 from create_ranking_table('Earth') ;
+
+select 1 from create_ranking_table('Africa') ;
+select 1 from create_ranking_table('Americas');
+select 1 from create_ranking_table('Asia');
+select 1 from create_ranking_table('Europe');
+select 1 from create_ranking_table('Oceania');
+
+select 1 from create_ranking_table( 'Country_ES');
+select 1 from create_ranking_table( 'Country_GB');
+select 1 from create_ranking_table( 'Country_US');
+
+
+drop function if exists create_ranking_table() ;
