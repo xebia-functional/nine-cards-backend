ead1f2a72ba28bfb3922f122e89a5b71e2b6d7b9
Diego Esteban Alonso Blas <diesalbla@gmail.com>
2016-08-01 21:10:55 +0200

    9C-470 Fix Typos.
    It seems that postgresql tables do not work fine if the names are
    capitals letters. Note that here we use the real value, whereas in
    the _v1 file it uses words that are automatically lower-cased.

--- modules/api/src/main/resources/db/migration/V2__Rankings_tables.sql
+++ modules/api/src/main/resources/db/migration/V2__Rankings_tables.sql
@@ -2,30 +2,30 @@
 * We found the basis for this solution in
 * http://dba.stackexchange.com/questions/42924/postgresql-function-to-create-table
 */
-create or replace function create_ranking_table(geo_scope varchar(30)) returns VOID as $$
+create function create_ranking_table(geo_scope varchar(30)) returns VOID as $$
 begin
   execute format(
-    'create table if not exists %I (
+      'create table if not exists %I (
         packageName character varying(256) not null,
         category varchar(30) not null,
         ranking integer not null,
         primary key (packageName, category)
     )',
-    'Rankings_' || geo_scope
+    'rankings_' || geo_scope
     );
 end
 $$ LANGUAGE plpgsql;
 
-select 1 from create_ranking_table('Earth') ;
+select 1 from create_ranking_table('earth') ;
 
-select 1 from create_ranking_table('Africa') ;
-select 1 from create_ranking_table('Americas');
-select 1 from create_ranking_table('Asia');
-select 1 from create_ranking_table('Europe');
-select 1 from create_ranking_table('Oceania');
+select 1 from create_ranking_table('africa') ;
+select 1 from create_ranking_table('americas');
+select 1 from create_ranking_table('asia');
+select 1 from create_ranking_table('europe');
+select 1 from create_ranking_table('oceania');
 
-select 1 from create_ranking_table('Country_ES');
-select 1 from create_ranking_table('Country_GB');
-select 1 from create_ranking_table('Country_US');
+select 1 from create_ranking_table('country_es');
+select 1 from create_ranking_table('country_gb');
+select 1 from create_ranking_table('country_us');
 
 drop function if exists create_ranking_table(varchar(30));
