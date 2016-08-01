f60a2a20bea0f120a31fd5a86eeaf6bcc7d8bc1b
Diego Esteban Alonso Blas <diesalbla@gmail.com>
2016-08-01 18:54:53 +0200

    9C-470 Some corrections
    We remove some extra spaces. We correct a bug in the drop function
    instruction. We change the scala version in travis, to see if that
    has some effect on the error we noticed in travis.

--- modules/api/src/main/resources/db/migration/V2__Rankings_tables.sql
+++ modules/api/src/main/resources/db/migration/V2__Rankings_tables.sql
@@ -1,9 +1,12 @@
-
-create or replace function create_ranking_table( geo_scope varchar(30)) returns VOID  as $$
+/*
+* We found the basis for this solution in
+* http://dba.stackexchange.com/questions/42924/postgresql-function-to-create-table
+*/
+create or replace function create_ranking_table(geo_scope varchar(30)) returns VOID as $$
 begin
   execute format(
-    ' create table if not exists %I (
-        packageName character  varying(256) not null,
+    'create table if not exists %I (
+        packageName character varying(256) not null,
         category varchar(30) not null,
         ranking integer not null,
         primary key (packageName, category)
@@ -21,9 +24,8 @@ select 1 from create_ranking_table('Asia');
 select 1 from create_ranking_table('Europe');
 select 1 from create_ranking_table('Oceania');
 
-select 1 from create_ranking_table( 'Country_ES');
-select 1 from create_ranking_table( 'Country_GB');
-select 1 from create_ranking_table( 'Country_US');
+select 1 from create_ranking_table('Country_ES');
+select 1 from create_ranking_table('Country_GB');
+select 1 from create_ranking_table('Country_US');
 
-
-drop function if exists create_ranking_table() ;
+drop function if exists create_ranking_table(varchar(30));
