e29064b0704ca63049def7729ffa218d09ee29da
franciscodr <francisco.d@47deg.com>
2016-09-20 16:59:33 +0200

    9C-562: Addresses code review comments

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,11 +19,7 @@ trait Dependencies {
   val scalazConcurrent = "org.scalaz" %% "scalaz-concurrent" % Versions.scalaz
   val jodaConvert = "org.joda" % "joda-convert" % Versions.jodaConvert
   val jodaTime = "joda-time" % "joda-time" % Versions.jodaTime
-  val doobieCore = "org.tpolecat" %% "doobie-core" % Versions.doobie exclude("org.scalaz", "scalaz-concurrent")
-  val doobieH2 = "org.tpolecat" %% "doobie-contrib-h2" % Versions.doobie exclude("org.scalaz", "scalaz-concurrent")
-  val doobieHikari = "org.tpolecat" %% "doobie-contrib-hikari" % Versions.doobie exclude("org.scalaz", "scalaz-concurrent")
-  val doobiePostgresql = "org.tpolecat" %% "doobie-contrib-postgresql" % Versions.doobie exclude("org.scalaz", "scalaz-concurrent")
-  val doobieSpecs2 = "org.tpolecat" %% "doobie-contrib-specs2" % Versions.doobie exclude("org.scalaz", "scalaz-concurrent")
+  def doobie(suff: String) = "org.tpolecat" %% s"doobie$suff" % Versions.doobie exclude("org.scalaz", "*")
   val typesafeConfig = "com.typesafe" % "config" % Versions.typesafeConfig
   val flywaydbCore = "org.flywaydb" % "flyway-core" % Versions.flywaydb
   val scalacheckShapeless = "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % Versions.scalacheckShapeless
@@ -64,11 +60,11 @@ trait Dependencies {
     jodaConvert,
     jodaTime,
     cats,
-    doobieCore exclude("org.scalaz", "*"),
-    doobieH2,
-    doobieHikari,
-    doobiePostgresql,
-    doobieSpecs2 % "test",
+    doobie("-core"),
+    doobie("-contrib-h2"),
+    doobie("-contrib-hikari"),
+    doobie("-contrib-postgresql"),
+    doobie("-contrib-specs2") % "test",
     enumeratum(""),
     enumeratum("-circe"),
     sprayJson,
