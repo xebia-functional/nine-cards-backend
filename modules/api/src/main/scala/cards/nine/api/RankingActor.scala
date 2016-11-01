50c9edb717f69c4243d01a41080758661575146c
franciscodr <francisco.d@47deg.com>
2016-11-01 21:34:39 +0100

    9C-999: Removing RankingGenerator class

--- modules/api/src/main/scala/cards/nine/api/RankingActor.scala
+++ modules/api/src/main/scala/cards/nine/api/RankingActor.scala
@@ -17,50 +17,45 @@ class RankingActor[F[_]](interpreter: F ~> Task)(implicit rankingProcesses: Rank
   implicit val system = ActorSystem("on-spray-can")
   val log = Logging(system, getClass)
 
-  class RankingGenerator {
+  private[this] def generateRankings = {
     val countriesPerRequest = nineCardsConfiguration.rankings.countriesPerRequest
     val maxNumberOfAppsPerCategory = nineCardsConfiguration.rankings.maxNumberOfAppsPerCategory
     val rankingPeriod = nineCardsConfiguration.rankings.rankingPeriod
 
-    def generateRankings = {
-      val now = DateTime.now(DateTimeZone.UTC)
-      val today = now.withTimeAtStartOfDay
+    val now = DateTime.now(DateTimeZone.UTC)
+    val today = now.withTimeAtStartOfDay
 
-      now.getDayOfWeek
-      val pageNumber = ((now.getDayOfWeek - 1) * 24 + now.getHourOfDay) * countriesPerRequest
+    val pageNumber = ((now.getDayOfWeek - 1) * 24 + now.getHourOfDay) * countriesPerRequest
 
-      rankingProcesses.reloadRankingForCountries(
-        RankingParams(
-          DateRange(
-            today.minus(rankingPeriod),
-            today
-          ),
-          maxNumberOfAppsPerCategory,
-          AnalyticsToken("") //TODO: We should generate a valid token
+    rankingProcesses.reloadRankingForCountries(
+      RankingParams(
+        DateRange(
+          today.minus(rankingPeriod),
+          today
         ),
-        Page(pageNumber, countriesPerRequest)
-      )
-    }
+        maxNumberOfAppsPerCategory,
+        AnalyticsToken("") //TODO: We should generate a valid token
+      ),
+      Page(pageNumber, countriesPerRequest)
+    )
   }
 
-  val rankingGenerator = new RankingGenerator()
-
-  def showCountriesWithoutRankingInfo(countriesCode: List[CountryIsoCode]) =
+  private[this] def showCountriesWithoutRankingInfo(countriesCode: List[CountryIsoCode]) =
     if (countriesCode.nonEmpty)
       log.info(s"Skipped countries without ranking info: ${countriesCode.map(_.value).mkString(",")}")
 
-  def showRankingGenerationInfo(summary: UpdateRankingSummary) = summary.countryCode match {
-    case None ⇒ log.info(s"World ranking generated: ${summary.created} entries created")
-    case Some(code) ⇒ log.info(s"Ranking generated for ${code.value}: ${summary.created} entries created")
-  }
+  private[this] def showRankingGenerationInfo(summary: UpdateRankingSummary) =
+    summary.countryCode match {
+      case None ⇒ log.info(s"World ranking generated: ${summary.created} entries created")
+      case Some(code) ⇒ log.info(s"Ranking generated for ${code.value}: ${summary.created} entries created")
+    }
 
   def receive = {
     case RankingByCategory ⇒
       log.info("Running actor for generate rankings ...")
-      rankingGenerator.generateRankings.foldMap(interpreter).unsafePerformAsync(
+      generateRankings.foldMap(interpreter).unsafePerformAsync(
         _.fold(
-          e ⇒ log.error(e, "An error was found while generating rankings"),
-          {
+          e ⇒ log.error(e, "An error was found while generating rankings"), {
             case Left(e) ⇒ log.error("An error was found while generating rankings")
             case Right(summary) ⇒
               showCountriesWithoutRankingInfo(summary.countriesWithoutRanking)
