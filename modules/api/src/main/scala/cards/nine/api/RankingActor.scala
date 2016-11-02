e33175b061e8c261ebde1a1239273a666193f57d
franciscodr <francisco.d@47deg.com>
2016-11-02 10:25:44 +0100

    9C-999: Addresses review comments

--- modules/api/src/main/scala/cards/nine/api/RankingActor.scala
+++ modules/api/src/main/scala/cards/nine/api/RankingActor.scala
@@ -1,16 +1,20 @@
 package cards.nine.api
 
-import akka.actor.{ Actor, ActorSystem }
+import akka.actor.{Actor, ActorSystem}
 import akka.event.Logging
 import cards.nine.api.RankingActor.RankingByCategory
+import cards.nine.commons.NineCardsService.Result
 import cards.nine.commons.TaskInstances._
 import cards.nine.commons.config.NineCardsConfig._
 import cards.nine.domain.analytics._
 import cards.nine.domain.pagination.Page
 import cards.nine.processes.RankingProcesses
+import cards.nine.processes.messages.rankings.Reload.SummaryResponse
 import cats.~>
-import org.joda.time.{ DateTime, DateTimeZone }
+import doobie.enum.resultsetconcurrency.ResultSetConcurrency
+import org.joda.time.{DateTime, DateTimeZone}
 
+import scalaz.{-\/, \/, \/-}
 import scalaz.concurrent.Task
 
 class RankingActor[F[_]](interpreter: F ~> Task)(implicit rankingProcesses: RankingProcesses[F]) extends Actor {
@@ -40,11 +44,22 @@ class RankingActor[F[_]](interpreter: F ~> Task)(implicit rankingProcesses: Rank
     )
   }
 
+  private[this] def showRankingGenerationInfo(result: Throwable \/ Result[SummaryResponse]) = {
+    result match {
+      case -\/(e) => log.error(e, "An error was found while generating rankings")
+      case \/-(Left(e)) => log.error("An error was found while generating rankings")
+      case \/-(Right(summary)) =>
+        showCountriesWithoutRankingInfo(summary.countriesWithoutRanking)
+        summary.countriesWithRanking foreach showRankingSummary
+        log.info("Rankings generated successfully")
+    }
+  }
+
   private[this] def showCountriesWithoutRankingInfo(countriesCode: List[CountryIsoCode]) =
     if (countriesCode.nonEmpty)
       log.info(s"Skipped countries without ranking info: ${countriesCode.map(_.value).mkString(",")}")
 
-  private[this] def showRankingGenerationInfo(summary: UpdateRankingSummary) =
+  private[this] def showRankingSummary(summary: UpdateRankingSummary) =
     summary.countryCode match {
       case None ⇒ log.info(s"World ranking generated: ${summary.created} entries created")
       case Some(code) ⇒ log.info(s"Ranking generated for ${code.value}: ${summary.created} entries created")
@@ -53,18 +68,7 @@ class RankingActor[F[_]](interpreter: F ~> Task)(implicit rankingProcesses: Rank
   def receive = {
     case RankingByCategory ⇒
       log.info("Running actor for generate rankings ...")
-      generateRankings.foldMap(interpreter).unsafePerformAsync(
-        _.fold(
-          e ⇒ log.error(e, "An error was found while generating rankings"), {
-            case Left(e) ⇒ log.error("An error was found while generating rankings")
-            case Right(summary) ⇒
-              showCountriesWithoutRankingInfo(summary.countriesWithoutRanking)
-              summary.countriesWithRanking foreach showRankingGenerationInfo
-              log.info("Rankings generated successfully")
-          }
-
-        )
-      )
+      generateRankings.foldMap(interpreter).unsafePerformAsync(showRankingGenerationInfo)
   }
 }
 
