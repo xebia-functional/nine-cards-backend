86d757eaa7dd8fa57eeb99b1e9ded427c3471f85
Javi Pacheco <javi@47deg.com>
2017-01-24 15:03:53 +0100

    New routes for TOS and Shared Collections

--- modules/api/src/main/scala/cards/nine/api/NineCardsApi.scala
+++ modules/api/src/main/scala/cards/nine/api/NineCardsApi.scala
@@ -1,16 +1,19 @@
 package cards.nine.api
 
 import akka.actor.ActorRefFactory
+import cards.nine.api.NineCardsHeaders.Domain.PublicIdentifier
 import cards.nine.api.applications.ApplicationsApi
 import cards.nine.api.collections.CollectionsApi
 import cards.nine.api.rankings.RankingsApi
 import cards.nine.api.accounts.AccountsApi
+import cards.nine.api.utils.SprayMatchers.TypedSegment
 import cards.nine.commons.config.Domain.NineCardsConfiguration
 import cards.nine.processes._
 import cards.nine.processes.account.AccountProcesses
 import cards.nine.processes.applications.ApplicationProcesses
 import cards.nine.processes.rankings.RankingProcesses
 import cards.nine.processes.NineCardsServices._
+
 import scala.concurrent.ExecutionContext
 import spray.routing._
 
@@ -28,6 +31,10 @@ class NineCardsRoutes(
 
   lazy val nineCardsRoutes: Route =
     pathPrefix("apiDocs")(swaggerRoute) ~
+      pathPrefix("tos")(tosRoute) ~
+      pathPrefix("shared-collection" / TypedSegment[PublicIdentifier]) {
+        _ ⇒ sharedCollectionRoute
+      } ~
       (new AccountsApi().route) ~
       (new ApplicationsApi().route) ~
       (new CollectionsApi().route) ~
@@ -41,5 +48,13 @@ class NineCardsRoutes(
     } ~ {
       getFromResourceDirectory("apiDocs")
     }
+
+  private[this] lazy val tosRoute: Route =
+    pathEnd {
+      getFromResource("web/tos.html")
+    }
+
+  private[this] lazy val sharedCollectionRoute: Route =
+    getFromResource("web/collection.html")
 
 }
