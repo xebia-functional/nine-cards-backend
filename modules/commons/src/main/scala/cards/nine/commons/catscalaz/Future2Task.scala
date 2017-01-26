bea8cf486d4f3f436496a0843fd5f8a13b1c9a64
Diego Esteban Alonso Blas <diego.e.a@47deg.com>
2017-01-26 17:43:11 +0100

    9C-1237 Attribution notice, for code from Stack Overflow

--- modules/commons/src/main/scala/cards/nine/commons/catscalaz/Future2Task.scala
+++ modules/commons/src/main/scala/cards/nine/commons/catscalaz/Future2Task.scala
@@ -1,20 +1,20 @@
+//
+// Source code from Pavel Chlupacek in http://stackoverflow.com/a/17377768/1002111
+// published under CC-SA license.
+//
+// Modified to use the Scalaz std library
+//
 package cards.nine.commons.catscalaz
 
 import scala.concurrent.{ ExecutionContext, Future }
-import scala.util.{ Failure, Success }
 import scalaz.concurrent.Task
-import scalaz.{ \/-, -\/ }
+import scalaz.std.{ `try` ⇒ TryF }
 
 object ScalaFuture2Task {
 
   def apply[T](fut: Future[T])(implicit ec: ExecutionContext): Task[T] =
-    Task.async {
-      register ⇒
-        fut.onComplete {
-          case Success(v) ⇒ register(\/-(v))
-          case Failure(ex) ⇒ register(-\/(ex))
-        }
+    Task.async { cont ⇒
+      fut.onComplete(x ⇒ cont(TryF.toDisjunction(x)))
     }
 
 }
-
